efde1f74bd82a34ddbe40779d99debf5bb46043f
Marco Elizalde <m.elizalde.torres@gmail.com>
2020-11-25 06:31:53 -0600

    feat create new activity splash

---
+++ app/src/main/java/com/marcoelizalde/platziconf/ui/SplashscreenActivity.kt
@@ -0,0 +1,12 @@
+package com.marcoelizalde.platziconf.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.marcoelizalde.platziconf.R
+
+class SplashscreenActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splashscreen)
+    }
+}
