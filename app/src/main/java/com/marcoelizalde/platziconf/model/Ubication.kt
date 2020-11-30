db498674b0a11e4ba19c27217a214e30dda42b3f
Marco Elizalde <m.elizalde.torres@gmail.com>
2020-11-30 06:50:26 -0600

    add firebase al proyecto

---
+++ app/src/main/java/com/marcoelizalde/platziconf/model/Ubication.kt
@@ -0,0 +1,11 @@
+package com.marcoelizalde.platziconf.model
+
+class Ubication {
+    val name = ""
+    var address = ""
+    val latitude = ""
+    val longitude = ""
+    val phone = ""
+    val website = ""
+    val photo = ""
+}
