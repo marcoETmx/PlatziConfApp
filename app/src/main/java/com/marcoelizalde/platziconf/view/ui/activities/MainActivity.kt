939d51c2a0b3b72036850562ac0c2c52a7df744d
Marco Elizalde <m.elizalde.torres@gmail.com>
2020-11-30 12:32:10 -0600

    refactor

--- app/src/main/java/com/marcoelizalde/platziconf/view/ui/activities/MainActivity.kt
+++ app/src/main/java/com/marcoelizalde/platziconf/view/ui/activities/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.marcoelizalde.platziconf
+package com.marcoelizalde.platziconf.view.ui.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.marcoelizalde.platziconf.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
--- app/src/main/java/com/marcoelizalde/platziconf/view/ui/fragments/ScheduleFragment.kt
+++ app/src/main/java/com/marcoelizalde/platziconf/view/ui/fragments/ScheduleFragment.kt
@@ -1,4 +1,4 @@
-package com.marcoelizalde.platziconf.ui.fragments
+package com.marcoelizalde.platziconf.view.ui.fragments
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
