214047e78b6ea222b661734b3e730eb7ddd2145a
Marco Elizalde <m.elizalde.torres@gmail.com>
2020-11-30 18:41:53 -0600

    add(schedule) se implementa vienw modwl en el frangmento de speaker

--- app/src/main/java/com/marcoelizalde/platziconf/view/ui/fragments/SpeakersFragment.kt
+++ app/src/main/java/com/marcoelizalde/platziconf/view/ui/fragments/SpeakersFragment.kt
@@ -5,16 +5,66 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.os.bundleOf
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.GridLayoutManager
 import com.marcoelizalde.platziconf.R
+import com.marcoelizalde.platziconf.model.Speaker
+import com.marcoelizalde.platziconf.view.adapter.SpeakerAdapter
+import com.marcoelizalde.platziconf.view.adapter.SpeakerListener
+import com.marcoelizalde.platziconf.viewmodel.SpeakersViewModel
+import kotlinx.android.synthetic.main.fragment_speakers.*
+import androidx.navigation.fragment.findNavController
 
-class SpeakersFragment : Fragment() {
 
+class SpeakersFragment : Fragment() , SpeakerListener {
+
+    private lateinit var speakerAdapter: SpeakerAdapter
+
+    private lateinit var viewModel: SpeakersViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_speakers, container, false)
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        viewModel =
+            ViewModelProvider(this)
+                .get(SpeakersViewModel::class.java)
+
+        viewModel.refresh()
+
+        speakerAdapter = SpeakerAdapter(this)
+
+        rvSpeakers.apply {
+            layoutManager = GridLayoutManager(view.context, 2)
+            adapter = speakerAdapter
+        }
+
+        observeViewModel()
+    }
+
+    fun observeViewModel() {
+        viewModel.listSpeakers.observe(viewLifecycleOwner,
+            Observer<List<Speaker>> { speakers ->
+                speakerAdapter.updateData(speakers)
+            })
+
+        viewModel.isLoading.observe(viewLifecycleOwner, Observer<Boolean> {
+            if (it != null)
+                rlBaseSpeaker.visibility = View.INVISIBLE
+        })
+    }
+
+    override fun onSpeakerClicked(speaker: Speaker, position: Int) {
+        val bundle = bundleOf("speaker" to speaker)
+        findNavController().navigate(R.id.speakersDetailFragmentDialog, bundle)
+    }
+
 }
